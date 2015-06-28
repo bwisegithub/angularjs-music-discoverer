a1882181e543a1685aab58c7532a35072625ab97
bwisegithub <bwisegithub@users.noreply.github.com>
2015-06-27 22:00:31 -0400

    Test if angular works on remote

---
+++ javascripts/moby.js
@@ -0,0 +1,31 @@
+angular.module('ngView', [], function($routeProvider, $locationProvider) {
+  console.log($routeProvider, $locationProvider);
+  $routeProvider.when('/Book/:bookId', {
+    templateUrl: 'book.html',
+    controller: BookCntl
+  }).when('/Book/:bookId/ch/:chapterId', {
+    templateUrl: 'chapter.html',
+    controller: ChapterCntl
+  });
+
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
+});
+
+function MainCntl($scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+}
+
+function BookCntl($scope, $routeParams) {
+  console.log($scope, $routeParams);
+  $scope.name = "BookCntl";
+  $scope.params = $routeParams;
+}
+
+function ChapterCntl($scope, $routeParams) {
+  console.log($scope, $routeParams);
+  $scope.name = "ChapterCntl";
+  $scope.params = $routeParams;
+}
