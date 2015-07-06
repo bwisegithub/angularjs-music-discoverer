1a43928b4a35064a126c55ab9d285865ba8bcb85
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-05 22:14:51 -0400

    Push to test dbpedia service on remote

--- js/services/DbpediaService.js
+++ js/services/DbpediaService.js
@@ -1,9 +1,24 @@
 app.factory('musicians', ['$http', function($http) {
-	return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json')
+
+	var url = "http://dbpedia.org/sparql/";
+	var query = [
+ 		"select distinct ?Concept where {[] a ?Concept} ",
+ 		"LIMIT 100"
+	].join(" ");
+
+	var queryUrl = url+"?query="+ encodeURIComponent(query) +"&format=json";
+	//queryUrl = 'https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json';
+	window.alert(queryUrl);
+
+	return $http.get(queryUrl)
 		.success(function(data) {
+			//window.alert(data);
 		  return data;
 		})
 		.error(function(err) {
+			window.alert(err);
 		  return err;
 		});
 }]);
+
+
