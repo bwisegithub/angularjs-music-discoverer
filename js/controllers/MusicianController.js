8c78dd9fd8247f6cfb6003f13e776264b5a70c26
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-07 16:51:05 -0400

    Get and display associated musicians

--- js/controllers/MusicianController.js
+++ js/controllers/MusicianController.js
@@ -2,7 +2,7 @@ app.controller('MusicianController', ['$scope', '$routeParams', 'sparqlQueries',
 
 	$scope.controllerData = { headerKey: 'Musician Details' };
 
-	// Get the appropriate sparql query string
+	// Get the sparql query string for the musician
 	// then execute it on dbpedia for the given routeParams id
 	sparqlQueries.getData().success(function(data) {
 		var sparqlQuery = sparqlQueries.getQueryStr(data, 'musicianDetailsQuery');
@@ -12,10 +12,33 @@ app.controller('MusicianController', ['$scope', '$routeParams', 'sparqlQueries',
 			sparqlQuery = sparqlQuery.replace('--REPLACE_ID--', $routeParams.id);
 
 			dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
-				$scope.dbpResults = data.results.bindings;
+				$scope.dbpResultsMusician = data.results.bindings;
+				if ($scope.dbpResultsMusician.length > 0) {
+					var musicianResource = $scope.dbpResultsMusician[0].resource.value;
+
+					// Get the sparql query string for the musicians associates
+					// then execute it on dbpedia for the given musician
+					sparqlQueries.getData().success(function(data) {
+					var sparqlQuery = sparqlQueries.getQueryStr(data, 'musicianAssociatedQuery');
+
+						if ($routeParams.id && $routeParams.id > 0) {
+							// Substitute in the routeParams id (which is the dbpedia-owl:wikiPageID)
+							sparqlQuery = sparqlQuery.replace('--REPLACE_ID--', $routeParams.id);
+							sparqlQuery = sparqlQuery.replace(/--REPLACE_RESOURCE--/g, musicianResource);
+
+							dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
+								$scope.dbpResultsAssociates = data.results.bindings;
+							}); 
+						}
+
+					}); 
+				}
 			}); 
 		}		
-
 	}); 
 
+	$scope.isNotFeaturedMusician = function(associate) {
+		return !(associate.id.value === $routeParams.id);
+	};
+	
 }]);
