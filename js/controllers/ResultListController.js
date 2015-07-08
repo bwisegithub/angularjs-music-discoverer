7e6af340728dde425429073582492b014513c476
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-08 19:35:32 -0400

    Mod so if only one search result, redirect to musician details

--- js/controllers/ResultListController.js
+++ js/controllers/ResultListController.js
@@ -1,4 +1,4 @@
-app.controller('ResultListController', ['$scope', 'sparqlQueries', 'dbpResults', function($scope, sparqlQueries, dbpResults) {
+app.controller('ResultListController', ['$scope', '$location', 'sparqlQueries', 'dbpResults', function($scope, $location, sparqlQueries, dbpResults) {
 
 	$scope.controllerData = { headerKey: 'Search Result List' };
 
@@ -7,16 +7,26 @@ app.controller('ResultListController', ['$scope', 'sparqlQueries', 'dbpResults',
 	sparqlQueries.getData().success(function(data) {
 		var sparqlQuery = sparqlQueries.getQueryStr(data, 'keywordSearchQuery');
 
-		// get the keywords entered by user
+		// Get the keywords entered by user
 		var searchKeywords = dbpResults.getSearchKeywords();
 
 		if (searchKeywords && searchKeywords.length > 0) {
 			// Substitute in the search keywords.  
 			// Note replacing spaces with underscores in search string for SPARQL bif:contains function.
-			sparqlQuery = sparqlQuery.replace('--REPLACE_KEYWORDS--', searchKeywords.replace(' ','_'));
+			sparqlQuery = sparqlQuery.replace('--REPLACE_KEYWORDS--', searchKeywords.replace(/ /g,'_'));
 
 			dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
-				$scope.dbpResults = data.results.bindings;
+				var dbpResultsGetMusicianDetails = data.results.bindings;
+				var id = '';
+				if (dbpResultsGetMusicianDetails.length === 1) {
+					// There is only one result so just redirect to musician view 
+					// for the wikiPageId in that one result.
+					id = dbpResultsGetMusicianDetails[0].id.value;
+					$location.path('/musician/' + id);
+				} else {
+					// There are no, or more than one, result(s) so show result list.
+					$scope.dbpResults = data.results.bindings;
+				}
 			}); 		
 		}
 	}); 
