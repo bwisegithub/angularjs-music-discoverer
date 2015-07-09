329f4c8d1c731c2278cfdae6b1db99f7f31429a4
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-09 15:30:25 -0400

    Mod mdFallbackSrc directive to allow either none or designated fallback

--- js/directives/mdFallbackSrc.js
+++ js/directives/mdFallbackSrc.js
@@ -2,8 +2,16 @@ app.directive('mdFallbackSrc', function() {
 	return { 
 		restrict: 'A',
 		link: function postLink(scope, iElement, iAttrs) {
-		  	iElement.bind('error', function() {
-				angular.element(this).attr("src", iAttrs.mdFallbackSrc);
+			iElement.bind('error', function() {
+				// There was an error finding the img src
+				if (iAttrs.mdFallbackSrc === 'none') {
+					// Remove the image element entirely
+					angular.element(this).remove();
+				}
+				else {
+					// Use the iAttrs.mdFallbackSrc for the fallback img
+					angular.element(this).attr('src', iAttrs.mdFallbackSrc);
+				}
 			});
 		}
 	}; 
