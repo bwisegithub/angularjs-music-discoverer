46c8be617700229c2814aebc3f7bee450b093886
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-13 09:47:00 -0400

    Add connection timeout handling for DBpedia service (dbpResults)

--- js/services/DbpediaService.js
+++ js/services/DbpediaService.js
@@ -3,20 +3,28 @@ app.factory('dbpResults', ['$http', function($http) {
 	return {
 		getDbpediaResults: function(sparqlQuery) { 
 
+			var timeoutInt = 30000;
+			var startTime = new Date().getTime();
+
 			if (sparqlQuery != 'QUERY NOT FOUND') {
 				var url = 'http://dbpedia.org/sparql/';
 				var query_prefix = '?default-graph-uri=http%3A%2F%2Fdbpedia.org';
 				query = '&query=' + encodeURIComponent(sparqlQuery);
-				var query_suffix = '&format=application%2Fsparql-results%2Bjson&timeout=30000&debug=on';
+				var query_suffix = '&format=application%2Fsparql-results%2Bjson&timeout=' + timeoutInt + '&debug=on';
 				var queryUrl = url + query_prefix + query + query_suffix;
 
-				return $http.jsonp(queryUrl + '&callback=JSON_CALLBACK')
+				return $http.jsonp(queryUrl + '&callback=JSON_CALLBACK', {timeout: timeoutInt})
 					.success(function(data) {
 						return data;
 					})
-					.error(function(err) {
-						return err;
-					}); 
+					.error(function(resp, status, header, config) {
+						var respTime = new Date().getTime() - startTime;
+						if(respTime >= config.timeout) {
+							window.alert('Timeout error calling the dbpedia.org/sparql service.\nThe service may be under maintenance.\nTry again later.');
+						} else {
+							window.alert('Unexpected error calling the dbpedia.org/sparql service');
+						}
+					});
 			}
 		}
 	};
