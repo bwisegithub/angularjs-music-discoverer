98c9869f60a02afef7f5cd9a74fed35c2a8a1556
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-13 13:53:14 -0400

    Add tooltip for genre buttons with new mdTooltip directive

---
+++ app/directives/mdTooltip.js
@@ -0,0 +1,14 @@
+app.directive('mdTooltip', function(){
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs){
+			$(element).hover(function(){
+				// on mouseenter
+				$(element).tooltip('show');
+			}, function(){
+				// on mouseleave
+				$(element).tooltip('hide');
+			});
+		}
+	};
+});
