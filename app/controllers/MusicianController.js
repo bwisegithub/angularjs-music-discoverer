59e075c7612228c0504338bc899fe4c9e5e82a11
bwisegithub <bwisegithub@users.noreply.github.com>
2015-07-16 13:22:23 -0400

    Add routeParams validation

--- app/controllers/MusicianController.js
+++ app/controllers/MusicianController.js
@@ -6,68 +6,71 @@ app.controller('MusicianController', ['$scope', '$routeParams', 'sparqlQueries',
 	sparqlQueries.getData().success(function(data) {
 		var sparqlQuery = sparqlQueries.getQueryStr(data, 'musicianDetailsQuery');
 
-		if ($routeParams.id && $routeParams.id > 0) {
-			// Substitute in the routeParams id (which is the dbpedia-owl:wikiPageID)
-			sparqlQuery = sparqlQuery.replace('--REPLACE_ID--', $routeParams.id);
+		// Validate URL input before proceeding
+		if (/^[0-9]*$/.test($routeParams.id)) {
+			if ($routeParams.id > 0) {
+				// Substitute in the routeParams id (which is the dbpedia-owl:wikiPageID)
+				sparqlQuery = sparqlQuery.replace('--REPLACE_ID--', $routeParams.id);
 
-			// Get the main musician info
-			dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
-				$scope.dbpResultsMusician = data.results.bindings;
+				// Get the main musician info
+				dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
+					$scope.dbpResultsMusician = data.results.bindings;
 
-				if ($scope.dbpResultsMusician.length > 0) {
-					// Musician found
-					var musicianResource = $scope.dbpResultsMusician[0].resource.value;
+					if ($scope.dbpResultsMusician.length > 0) {
+						// Musician found
+						var musicianResource = $scope.dbpResultsMusician[0].resource.value;
 
-					// Get the sparql query string for the musicians associates
-					// then execute it on dbpedia for the given musician
-					sparqlQueries.getData().success(function(data) {
-					var sparqlQuery = sparqlQueries.getQueryStr(data, 'musicianAssociatedQuery');
+						// Get the sparql query string for the musicians associates
+						// then execute it on dbpedia for the given musician
+						sparqlQueries.getData().success(function(data) {
+						var sparqlQuery = sparqlQueries.getQueryStr(data, 'musicianAssociatedQuery');
 
-						if ($routeParams.id && $routeParams.id > 0) {
-							// Substitute in the routeParams id (which is the dbpedia-owl:wikiPageID)
-							sparqlQuery = sparqlQuery.replace('--REPLACE_ID--', $routeParams.id);
-							sparqlQuery = sparqlQuery.replace(/--REPLACE_RESOURCE--/g, musicianResource);
+							if ($routeParams.id && $routeParams.id > 0) {
+								// Substitute in the routeParams id (which is the dbpedia-owl:wikiPageID)
+								sparqlQuery = sparqlQuery.replace('--REPLACE_ID--', $routeParams.id);
+								sparqlQuery = sparqlQuery.replace(/--REPLACE_RESOURCE--/g, musicianResource);
 
-							// Get the associated musician info
-							dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
-								// Make two copy of the results
-								// orig - As is. Will use this in d3.js diagrams.
-								//        Includes the featured artist.
-								// filteredCopy - Remove the featured artist.
-								//        Use in associated musicians carousel.
-								//        (Tried using filter in expression for
-								//        carousel, but tricky because of wanting
-								//        to show two slides at a time in the
-								//        carousel so needing direct access to 
-								//        the object index and didn't know which
-								//        one was filtered out.) 
-								
-								var orig = data.results.bindings;
-								// Make *copy* of results:
-								var filteredCopy = orig.slice(0);
-								var indexToRemove = -1;
+								// Get the associated musician info
+								dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
+									// Make two copy of the results
+									// orig - As is. Will use this in d3.js diagrams.
+									//        Includes the featured artist.
+									// filteredCopy - Remove the featured artist.
+									//        Use in associated musicians carousel.
+									//        (Tried using filter in expression for
+									//        carousel, but tricky because of wanting
+									//        to show two slides at a time in the
+									//        carousel so needing direct access to 
+									//        the object index and didn't know which
+									//        one was filtered out.) 
+									
+									var orig = data.results.bindings;
+									// Make *copy* of results:
+									var filteredCopy = orig.slice(0);
+									var indexToRemove = -1;
 
-								for (var i=0; i<filteredCopy.length; i++) {
-									// See if the featured artist is in the associated
-									// array (most likely it is but in theory may not
-									// be if more than 100 associates)
-									if (filteredCopy[i].id.value === $routeParams.id) {
-										indexToRemove = i;
+									for (var i=0; i<filteredCopy.length; i++) {
+										// See if the featured artist is in the associated
+										// array (most likely it is but in theory may not
+										// be if more than 100 associates)
+										if (filteredCopy[i].id.value === $routeParams.id) {
+											indexToRemove = i;
+										}
 									}
-								}
 
-								if (indexToRemove != -1) {
-									// Remove featured artist from filteredCopy
-									filteredCopy.splice(indexToRemove, 1);
-								}
+									if (indexToRemove != -1) {
+										// Remove featured artist from filteredCopy
+										filteredCopy.splice(indexToRemove, 1);
+									}
 
-								$scope.dbpResultsAssociatesInclFeatured = orig;
-								$scope.dbpResultsAssociates = filteredCopy;
-							}); 
-						}
-					}); 
-				}
-			}); 
+									$scope.dbpResultsAssociatesInclFeatured = orig;
+									$scope.dbpResultsAssociates = filteredCopy;
+								}); 
+							}
+						}); 
+					}
+				}); 
+			}
 		}		
 	}); 
 }]);
--- app/controllers/ResultListController.js
+++ app/controllers/ResultListController.js
@@ -6,28 +6,31 @@ app.controller('ResultListController', ['$scope', '$routeParams', '$location', '
 	sparqlQueries.getData().success(function(data) {
 		var sparqlQuery = sparqlQueries.getQueryStr(data, 'keywordSearchQuery');
 
-		// Get the keywords entered by user
-		var searchKeywords = $routeParams.searchKeywords;
+		// Already checked against regex for gui input, but check for input via 
+		// other means that have bypassed GUI e.g. url input
+		if (/^[a-zA-Z0-9\s]*$/.test($routeParams.searchKeywords)) {
+			// Get the keywords entered by user
+			var searchKeywords = $routeParams.searchKeywords;
 
-		if (searchKeywords && searchKeywords.length > 0) {
-			// Substitute in the search keywords.  
-			// Note replacing spaces with underscores in search string for SPARQL bif:contains function.
-			sparqlQuery = sparqlQuery.replace('--REPLACE_KEYWORDS--', searchKeywords.replace(/ /g,'_'));
+			if (searchKeywords && searchKeywords.length > 0) {
+				// Substitute in the search keywords.  
+				// Note replacing spaces with underscores in search string for SPARQL bif:contains function.
+				sparqlQuery = sparqlQuery.replace('--REPLACE_KEYWORDS--', searchKeywords.replace(/ /g,'_'));
 
-			dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
-				var dbpResultsGetMusicianDetails = data.results.bindings;
-				var id = '';
-				if (dbpResultsGetMusicianDetails.length === 1) {
-					// There is only one result so just redirect to musician view 
-					// for the wikiPageId in that one result.
-					id = dbpResultsGetMusicianDetails[0].id.value;
-					$location.path('/musician/' + id);
-				} else {
-					// There are no, or more than one, result(s) so show result list.
-					$scope.dbpResults = data.results.bindings;
-				}
-			}); 		
-		}
+				dbpResults.getDbpediaResults(sparqlQuery).success(function(data) {
+					var dbpResultsGetMusicianDetails = data.results.bindings;
+					var id = '';
+					if (dbpResultsGetMusicianDetails.length === 1) {
+						// There is only one result so just redirect to musician view 
+						// for the wikiPageId in that one result.
+						id = dbpResultsGetMusicianDetails[0].id.value;
+						$location.path('/musician/' + id);
+					} else {
+						// There are no, or more than one, result(s) so show result list.
+						$scope.dbpResults = data.results.bindings;
+					}
+				}); 		
+			}
+		} // Else don't process...you're up to no good.
 	}); 
-
 }]);
